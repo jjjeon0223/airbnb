c0c6165e75ab3d012a3e35913e70a4e784dc3fdf
James WooJae Jeon <wjsdnwo223@gmail.com>
2021-12-31 12:02:10 +0900

    final

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiampqZW9uMDIyMyIsImEiOiJja3h0czFiN3U2MTU4Mm5vY2lhc2hncG55In0.iAsyxyD5YnKoUwXZgZE41A",
+  },
 };
